2edf5a9b7853dad6dafc54f0ab7797e8f0cf7118
kppetrick <kppetrick@gmail.com>
2021-01-26 13:48:06 -0500

    Initial commit

---
+++ src/main/java/com/example/zodiaxbackend/ZodiaxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.zodiaxbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZodiaxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZodiaxBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zodiaxbackend/ZodiaxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zodiaxbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZodiaxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
